0f19dedb0d4b1985c74f78132f5b069fba05f308
Mark Vitale <vitalema@rose-hulman.edu>
2011-04-14 14:19:53 -0400

    git is stupid and wont let me pull.

--- Beer/Beer/Beer/Item.h
+++ Beer/Beer/Beer/Item.h
@@ -6,6 +6,7 @@ class Item {
 		Item (std::string desc, Item itemList[]);
 		Item (std::string desc);
 		string getDesc();
+		bool canInteractWithItem(Item i);
 	private:
 		string description;
 	//	static Item interactableItems[25];// have to set arbitrary size
