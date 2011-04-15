c9622fd34073c17d6fdfd61532af040b9cd28a11
Mark Vitale <vitalema@rose-hulman.edu>
2011-04-15 18:38:22 -0400

    updating the item and player tests

--- Beer/BeerTest/BeerTest/ItemTest.cpp
+++ Beer/BeerTest/BeerTest/ItemTest.cpp
@@ -5,6 +5,15 @@
 #include <string>
 
 
+Item i1 = Item("Item 1");
+Item i2 = Item("Item 2");
+Item i3 = Item("Item 3");
+Item i4 = Item("Item 4");
+
+i1.setInteractable({i2,i3});
+i2.setInteractable({i1,i4});
+i3.setInteractable({i1});
+
 BEGIN_TEST(testGetShortDescription)
   {
 	string s = "short description";
@@ -19,4 +28,33 @@ BEGIN_TEST(testGetLongDescription)
  	Item newItem = Item(s);
 	WIN_ASSERT_EQUAL(s, newItem.getDesc());
   }
+ END_TEST
+ 
+BEGIN_TEST(testIsInteractable0)
+ {
+	 WIN_ASSERT_TRUE(i1.canInteractWithItem(i2));
+ }
+ END_TEST
+
+BEGIN_TEST(testIsInteractable1)
+ {
+	 WIN_ASSERT_TRUE(i1.canInteractWithItem(i3));
+ }
+ END_TEST
+
+BEGIN_TEST(testIsInteractable2)
+ {
+	 WIN_ASSERT_FALSE(i1.canInteractWithItem(i4));
+ }
+ END_TEST
+
+BEGIN_TEST(testIsInteractable3)
+ {
+	 WIN_ASSERT_FALSE(i1.canInteractWithItem(i4));
+ }
+ END_TEST
+BEGIN_TEST(testIsInteractable4)
+ {
+	 WIN_ASSERT_TRUE(i2.canInteractWithItem(i4));
+ }
  END_TEST
--- Beer/BeerTest/BeerTest/PlayerTest.cpp
+++ Beer/BeerTest/BeerTest/PlayerTest.cpp
@@ -3,32 +3,47 @@
 
 string s = "Tyler";
 Player newPlayer = Player(s);
+Item item = Item("item");
+Item item2 = Item("item 2");
+
 BEGIN_TEST(testPlayerName) 
 {
 
 	WIN_ASSERT_EQUAL(s, newPlayer.getPlayerName());
 }
 END_TEST
-
-	BEGIN_TEST(testPlayerClickCount0) 
+BEGIN_TEST(testPlayerClickCount0) 
 {
 
 	WIN_ASSERT_EQUAL(0, newPlayer.getClickCount());
 }
 END_TEST
 
-	BEGIN_TEST(testPlayerClickCount1) 
+BEGIN_TEST(testPlayerClickCount1) 
 {
 	newPlayer.increaseClickCount();
 	WIN_ASSERT_EQUAL(1, newPlayer.getClickCount());
 }
 END_TEST
 
-	BEGIN_TEST(testPlayerClickCount10) 
+BEGIN_TEST(testPlayerClickCount10) 
 {
 	for (int i = 0; i <9; i++) {
 		newPlayer.increaseClickCount();
 	};
 	WIN_ASSERT_EQUAL(10, newPlayer.getClickCount());
+}
+END_TEST
+
+BEGIN_TEST(testInventory1)
+{
+	newPlayer.addItem(item);
+	WIN_ASSERT_TRUE(newPlayer.hasItem(item));
+}
+END_TEST
+
+BEGIN_TEST(testInventory2)
+{
+	WIN_ASSERT_FALSE(newPlayer.hasItem(item2));
 }
 END_TEST
